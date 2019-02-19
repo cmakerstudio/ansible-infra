ad6e8c19aeb1217088681262f6a0e54bdc609ebc
Laurent Dumont <laurentfdumont@gmail.com>
2019-02-19 18:19:57 -0500

    Add logic for inventory and group-vars

--- update_playbooks.sh
+++ update_playbooks.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+DIRECTORY=inventory
+
 #Install the roles from Ansible Galaxy.
 ansible-galaxy install --roles-path ./roles --force --role-file ansible-requirements/cmaker-requirements.yml
+
+#Pull the Inventory repository.
+if [ ! -d "$DIRECTORY" ]; then
+    git clone https://github.com/gzsierra/ansible-inventory.git inventory
+else
+    cd inventory
+    git pull https://github.com/gzsierra/ansible-inventory.git
+fi
